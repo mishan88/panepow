24ec11a374b8b4ab6b74bae05a8e4fea75d2124b
mishan88 <mishanhideaki88@gmail.com>
2022-01-18 20:17:27 +0900

    fix: menu ui

--- src/menu.rs
+++ src/menu.rs
@@ -3,7 +3,7 @@ use bevy::{app::AppExit, prelude::*};
 use bevy_ui_navigation::{
     components::FocusableButtonBundle,
     systems::{default_keyboard_input, InputMapping},
-    Focusable, NavEvent, NavMenu, NavRequest,
+    Focusable, NavEvent, NavMenu, NavRequest, NavigationPlugin,
 };
 
 pub struct MenuPlugin;
@@ -12,15 +12,26 @@ impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<ButtonColors>()
             .init_resource::<InputMapping>()
-            .add_system_set(SystemSet::on_enter(AppState::Menu).with_system(setup_menu))
+            .add_plugin(NavigationPlugin)
+            .add_system_set(
+                SystemSet::on_enter(AppState::Menu)
+                    .with_system(setup_camera)
+                    .with_system(setup_menu),
+            )
             .add_system_set(
                 SystemSet::on_update(AppState::Menu)
                     .with_system(go_to_game)
                     .with_system(default_keyboard_input)
-                    .with_system(button_system),
+                    .with_system(button_system)
+                    .with_system(visible_battle_mode_node)
+                    .with_system(invisible_battle_mode_node),
             )
             .add_system_set(SystemSet::on_exit(AppState::Menu).with_system(cleanup_menu));
     }
+}
+
+struct MenuData {
+    ui_root: Entity,
 }
 
 struct ButtonColors {
@@ -40,90 +51,189 @@ impl Default for ButtonColors {
 }
 
 #[derive(Component)]
+struct PlayerModeNode;
+
+#[derive(Component)]
+struct PlayerModeButton;
+
+#[derive(Component)]
+struct BattleModeNode;
+
+#[derive(Component)]
+struct BattleModeButton;
+
+#[derive(Component)]
 struct DifficultyButton;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
 
 fn setup_menu(
     mut commands: Commands,
     font_assets: Res<FontAssets>,
-    button_colors: Res<ButtonColors>,
     mut requests: EventWriter<NavRequest>,
 ) {
-    commands.spawn_bundle(UiCameraBundle::default());
-
+    // setup root
     let root_button = commands
         .spawn_bundle(FocusableButtonBundle::from(ButtonBundle {
             color: Color::NONE.into(),
             ..Default::default()
         }))
         .id();
-    let positions = [
-        (Vec2::new(10.0, 0.0), "EASY"),
-        (Vec2::new(30.0, 0.0), "NORMAL"),
-        (Vec2::new(50.0, 0.0), "HARD"),
-    ];
-
-    commands
+    let ui_root = commands
         .spawn_bundle(NodeBundle {
             style: Style {
+                flex_direction: FlexDirection::Row,
                 position_type: PositionType::Absolute,
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::FlexEnd,
+                size: Size::new(Val::Percent(50.0), Val::Percent(100.0)),
                 ..Default::default()
             },
             color: Color::NONE.into(),
             ..Default::default()
         })
         .insert(NavMenu::root())
+        .insert(PlayerModeNode)
         .push_children(&[root_button])
+        .id();
+    // setup player mode node
+    let one_player_mode = commands
+        .spawn_bundle(focusable_button(true))
+        .insert(PlayerModeButton)
         .with_children(|cmd| {
-            cmd.spawn_bundle(NodeBundle {
-                style: Style {
-                    position_type: PositionType::Absolute,
-                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                    ..Default::default()
-                },
-                color: Color::NONE.into(),
+            cmd.spawn_bundle(text(&font_assets, "1 Player", true));
+        })
+        .id();
+    let two_player_mode = commands
+        .spawn_bundle(focusable_button(true))
+        .insert(PlayerModeButton)
+        .with_children(|cmd| {
+            cmd.spawn_bundle(text(&font_assets, "2 Players", true));
+        })
+        .id();
+
+    let player_mode_node = commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::ColumnReverse,
+                position_type: PositionType::Relative,
+                size: Size::new(Val::Percent(20.0), Val::Percent(20.0)),
                 ..Default::default()
-            })
-            .insert(NavMenu::reachable_from(root_button));
-            for (idx, (pos, mode)) in positions.into_iter().enumerate() {
-                let position = Rect {
-                    left: Val::Percent(pos.x),
-                    top: Val::Percent(pos.y),
-                    ..Default::default()
-                };
-                let focusable_button = cmd
-                    .spawn_bundle(ButtonBundle {
-                        style: Style {
-                            size: Size::new(Val::Px(95.0), Val::Px(95.0)),
-                            position,
-                            position_type: PositionType::Absolute,
-                            ..Default::default()
-                        },
-                        color: button_colors.normal.into(),
-                        ..Default::default()
-                    })
-                    .with_children(|p| {
-                        p.spawn_bundle(TextBundle {
-                            text: Text::with_section(
-                                mode,
-                                TextStyle {
-                                    font: font_assets.font.clone(),
-                                    font_size: 30.0,
-                                    ..Default::default()
-                                },
-                                Default::default(),
-                            ),
-                            ..Default::default()
-                        });
-                    })
-                    .insert(Focusable::default())
-                    .insert(DifficultyButton)
-                    .id();
-                if idx == 0 {
-                    requests.send(NavRequest::FocusOn(focusable_button));
-                }
-            }
-        });
+            },
+            color: Color::NONE.into(),
+            ..Default::default()
+        })
+        .insert(NavMenu::reachable_from(root_button))
+        .insert(BattleModeNode)
+        .push_children(&[one_player_mode, two_player_mode])
+        .id();
+    requests.send(NavRequest::FocusOn(one_player_mode));
+
+    // setup one plyaer mode node
+    let one_player_endless = commands
+        .spawn_bundle(focusable_button(false))
+        .insert(BattleModeButton)
+        .with_children(|cmd| {
+            cmd.spawn_bundle(text(&font_assets, "endless", false));
+        })
+        .id();
+    let one_player_score_attack = commands
+        .spawn_bundle(focusable_button(false))
+        .insert(BattleModeButton)
+        .with_children(|cmd| {
+            cmd.spawn_bundle(text(&font_assets, "score attack", false));
+        })
+        .id();
+    let one_player_puzzle = commands
+        .spawn_bundle(focusable_button(false))
+        .insert(BattleModeButton)
+        .with_children(|cmd| {
+            cmd.spawn_bundle(text(&font_assets, "puzzle", false));
+        })
+        .id();
+    let one_player_vs_com = commands
+        .spawn_bundle(focusable_button(false))
+        .insert(BattleModeButton)
+        .with_children(|cmd| {
+            cmd.spawn_bundle(text(&font_assets, "vs com", false));
+        })
+        .id();
+    let one_player_mode_node = commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::ColumnReverse,
+                position_type: PositionType::Relative,
+                size: Size::new(Val::Percent(20.0), Val::Percent(20.0)),
+                ..Default::default()
+            },
+            visibility: Visibility { is_visible: false },
+            color: Color::NONE.into(),
+            ..Default::default()
+        })
+        .insert(NavMenu::reachable_from(one_player_mode))
+        .push_children(&[
+            one_player_endless,
+            one_player_score_attack,
+            one_player_puzzle,
+            one_player_vs_com,
+        ])
+        .id();
+
+    // TODO: setup 2player mode
+    let two_player_mode_node = commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                size: Size::new(Val::Auto, Val::Auto),
+                ..Default::default()
+            },
+            color: Color::NONE.into(),
+            ..Default::default()
+        })
+        .id();
+
+    commands.entity(ui_root).push_children(&[
+        player_mode_node,
+        one_player_mode_node,
+        two_player_mode_node,
+    ]);
+
+    commands.insert_resource(MenuData { ui_root });
+}
+
+fn focusable_button(is_visible: bool) -> FocusableButtonBundle {
+    FocusableButtonBundle::from(ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+            ..Default::default()
+        },
+        color: Color::GREEN.into(),
+        visibility: Visibility { is_visible },
+        ..Default::default()
+    })
+}
+
+fn text(font_assets: &Res<FontAssets>, label: &str, is_visible: bool) -> TextBundle {
+    TextBundle {
+        style: Style {
+            margin: Rect::all(Val::Px(10.0)),
+            align_items: AlignItems::FlexStart,
+            ..Default::default()
+        },
+        text: Text::with_section(
+            label,
+            TextStyle {
+                font: font_assets.font.clone(),
+                font_size: 30.0,
+                color: Color::BLACK,
+            },
+            Default::default(),
+        ),
+        visibility: Visibility { is_visible },
+        ..Default::default()
+    }
 }
 
 fn button_system(
@@ -137,6 +247,72 @@ fn button_system(
             *color = button_colors.active.into();
         } else {
             *color = button_colors.normal.into();
+        }
+    }
+}
+
+fn visible_battle_mode_node(
+    mut events: EventReader<NavEvent>,
+    to_battle_mode_button: Query<Entity, With<BattleModeButton>>,
+    from_player_mode_buttons: Query<Entity, With<PlayerModeButton>>,
+    mut battle_mode_buttons: Query<
+        (&mut Visibility, &Children),
+        (With<BattleModeButton>, Without<Text>),
+    >,
+    mut battle_mode_button_text: Query<&mut Visibility, With<Text>>,
+) {
+    for event in events.iter() {
+        match event {
+            NavEvent::FocusChanged { from, to } => {
+                if let Ok(_player_mode_button) = from_player_mode_buttons.get(*from.first()) {
+                    if let Ok(_battle_mode_button) = to_battle_mode_button.get(*to.first()) {
+                        for (mut button_visibility, children) in battle_mode_buttons.iter_mut() {
+                            button_visibility.is_visible = true;
+                            for &child in children.iter() {
+                                if let Ok(mut text_visibility) =
+                                    battle_mode_button_text.get_mut(child)
+                                {
+                                    text_visibility.is_visible = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn invisible_battle_mode_node(
+    mut events: EventReader<NavEvent>,
+    from_battle_mode_button: Query<Entity, With<BattleModeButton>>,
+    to_player_mode_button: Query<Entity, With<PlayerModeButton>>,
+    mut battle_mode_buttons: Query<
+        (&mut Visibility, &Children),
+        (With<BattleModeButton>, Without<Text>),
+    >,
+    mut battle_mode_button_text: Query<&mut Visibility, With<Text>>,
+) {
+    for event in events.iter() {
+        match event {
+            NavEvent::FocusChanged { from, to } => {
+                if let Ok(_) = from_battle_mode_button.get(*from.first()) {
+                    if let Ok(_) = to_player_mode_button.get(*to.first()) {
+                        for (mut button_visibility, children) in battle_mode_buttons.iter_mut() {
+                            button_visibility.is_visible = false;
+                            for &child in children.iter() {
+                                if let Ok(mut text_visibility) =
+                                    battle_mode_button_text.get_mut(child)
+                                {
+                                    text_visibility.is_visible = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
         }
     }
 }
@@ -162,4 +338,6 @@ fn go_to_game(
     }
 }
 
-fn cleanup_menu() {}
+fn cleanup_menu(mut commands: Commands, menu_data: Res<MenuData>) {
+    commands.entity(menu_data.ui_root).despawn_recursive();
+}
